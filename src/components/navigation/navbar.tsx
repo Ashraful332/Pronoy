1706d1c2bddb681f7449eddbf4b11662e84b3c51
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-27 20:47:41 +0600

    up

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Logo from "../../assets/icon.png";
+
 export default function NaveBar() {
       return (
             <div>
-                  Nav
+                  <div>
+                        <p>Home</p>
+                        <p>About me</p>
+                        <p>Work</p>
+                        <p>Photos</p>
+                        <p>contact</p>
+                  </div>
+                  <div>
+                        <Image src={Logo} alt="logo" />
+                  </div>
+                  <div></div>
             </div>
       )
 }
