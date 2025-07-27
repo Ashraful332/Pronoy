b92be4955662a39e1731357e27a7ce095b3a334c
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-27 21:05:45 +0600

    i dont know what is the provlem

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,20 +1,33 @@
 import Image from "next/image";
 import Logo from "../../assets/icon.png";
+import Menu from "../../assets/menu.svg";
+import "./nav.css"
+import Link from "next/link";
 
 export default function NaveBar() {
       return (
-            <div>
-                  <div>
-                        <p>Home</p>
-                        <p>About me</p>
-                        <p>Work</p>
-                        <p>Photos</p>
-                        <p>contact</p>
-                  </div>
-                  <div>
-                        <Image src={Logo} alt="logo" />
-                  </div>
-                  <div></div>
+      <nav className="max-w-[1170px] flex-row-center justify-between m-auto font-normal mt-[30px] mb-5 ">
+            <div className="hidden-nav-show gap-[27px] ">
+                  <Link href='/'>Home</Link>
+                  <Link href='/'>About me</Link>
+                  <Link href='/'>exprience</Link>
+                  <Link href='/'>Photos</Link>
+                  <Link href='/'>Contact</Link>
             </div>
+            <div>
+                  <Image src={Menu} alt="menu" className="w-6 h-6 text-white lg:hidden menu-svg-icon " />
+            </div>
+            <div>
+                  <Image src={Logo} alt="logo" className="w-[53px] h-[55px] " />
+            </div>
+            <div className="flex-row-center gap-5">
+                  <p className="hidden sm:flex">
+                        11:24 were open
+                  </p>
+                  <Link href='/' className="Table-Reservation text-sm sm:text-[16px] ">
+                        Table Reservation
+                  </Link>
+            </div>
+      </nav>
       )
 }
