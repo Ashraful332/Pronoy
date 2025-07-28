5af28db02a99cb9d5f5f3ecd05224773fff806cf
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-28 10:18:05 +0600

    education and work section is add

--- src/components/Home/Work.tsx
+++ src/components/Home/Work.tsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
 
 
 export default function Work(){
     return(
-        <div>
-            Work
+        <div className="flex-center">
+            <h2 className="text-center font-bold text-2xl mt-[var(--margin-high)] ">Experience</h2>
+            <p>My work of experience</p>
+            <div className="flex items-center gap-8">
+                <div className="w-[400px] h-[300px] border border-white py-8 px-10 ">
+                    <Image src="" alt="logo" />
+                    <h4 className="font-bold text-lg text-white mt-6 mb-2">lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, maxime.</p>
+                </div>
+                <div className="w-[400px] h-[300px] border border-white py-8 px-10 ">
+                    <Image src="" alt="logo" />
+                    <h4 className="font-bold text-lg text-white mt-6 mb-2">lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, maxime.</p>
+                </div>
+                <div className="w-[400px] h-[300px] border border-white py-8 px-10 ">
+                    <Image src="" alt="logo" />
+                    <h4 className="font-bold text-lg text-white mt-6 mb-2">lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, maxime.</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Home/education/big.tsx
+++ src/components/Home/education/big.tsx
@@ -10,64 +10,85 @@ export default function EducationBigScreen(){
             <div className="w-[26vw] ml-[18.5vw] h-[1.5px] bg-white "></div>
             <h3 className="text-2xl font-bold">My Education</h3>
         </div>
-        <div className="flex-center max-w-screen overflow-x-hidden ">
-            <div className="flex ">
-                <div className="w-[376px] ">
-                    {/* section 1 */}
-                    <div className="flex flex-col items-end mb-4">
-                        <div className="flex">
-                            <Image src={EducationLogo} alt="" className="education-svg " />
-                            <div className="w-5 h-1 bg-white"></div>
-                        </div>
-                        <div className="flex flex-col items-end mr-4">
-                            <h3 className="mt-5 text-white">Lorem</h3>
-                            <p className="text-[#ffffffb6] ">Jan 2023 - May 2024</p>
-                        </div>
+        <div className="flex-center relative">
+            <div>
+                <div className="flex flex-row items-start gap-8 mb-4 ">
+                    <div className="w-[370px] flex flex-col items-end ">
+                        <Image src={EducationLogo} alt="logo" className="education-svg" />
+                        <h3 className="text-xl text-[#ffffffda] font-bold ">loren</h3>
+                        <p className="text-sm text-[#f7f3f3b9] ">Jan 2023 - May 2024</p>
                     </div>
-                    {/* section 2 */}
-                    <div className="flex flex-col items-start mb-4">
-                        <div className="flex flex-row-reverse">
-                            <div className="w-5 h-1 bg-white"></div>
-                        </div>
-                        <div className="flex flex-col items-start ml-4">
-                            <h3 className="mt-5 text-lg text-white">Former Jr. IET Accounts Officer & Management</h3>
-                            <p className="text-[#ffffffb6] ">institution: Robi Axiata Ltd & Airtel</p>
-                            <p className="">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci consectetur possimus voluptates nostrum eveniet repellat perferendis rem odit commodi aliquam?
-                            </p>
-                        </div>
+                    <div className="w-4 h-1 bg-white "></div>
+                    <div className="w-[370px] flex flex-col items-start ">
+                        <h2 className="text-lg font-bold mb-2 text-white">Former Jr. IET Accounts Officer & Management</h2>
+                        <p className="mb-3 font-bold">institution: "Robi Axiata Ltd & Airtel"</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non totam laborum velit necessitatibus quidem fugit nesciunt corporis praesentium vero similique.
+                        </p>
                     </div>
                 </div>
-
-                <div className="w-[1.5px] h-[700px] bg-white "></div>
-
-                <div className="w-[376px] ">
-                    {/* section 1 */}
-                    <div className="flex flex-col items-start mb-4">
-                        <div className="flex flex-row-reverse">
-                            <div className="w-5 h-1 bg-white"></div>
-                        </div>
-                        <div className="flex flex-col items-start ml-4">
-                            <h3 className="mt-5 text-lg text-white">Former Jr. IET Accounts Officer & Management</h3>
-                            <p className="text-[#ffffffb6] ">institution: Robi Axiata Ltd & Airtel</p>
-                            <p className="">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci consectetur possimus voluptates nostrum eveniet repellat perferendis rem odit commodi aliquam?
-                            </p>
-                        </div>
+                <div className="flex flex-row-reverse items-start gap-8 mb-4 ">
+                    <div className="w-[370px] flex flex-col  ">
+                        <Image src={EducationLogo} alt="logo" className="education-svg" />
+                        <h3 className="text-xl text-[#ffffffda] font-bold ">loren</h3>
+                        <p className="text-sm text-[#f7f3f3b9] ">Jan 2023 - May 2024</p>
                     </div>
-                    {/* section 2 */}
-                    <div className="flex flex-col items-end mb-4">
-                        <div className="flex">
-                            <Image src={EducationLogo} alt="" className="education-svg " />
-                            <div className="w-5 h-1 bg-white"></div>
-                        </div>
-                        <div className="flex flex-col items-end mr-4">
-                            <h3 className="mt-5 text-white">Lorem</h3>
-                            <p className="text-[#ffffffb6] ">Jan 2023 - May 2024</p>
-                        </div>
+                    <div className="w-4 h-1 bg-white "></div>
+                    <div className="w-[370px] flex flex-col items-end text-end ">
+                        <h2 className="text-lg font-bold mb-2 text-white">Former Online Promoter, Assistant Director & Co-Artist</h2>
+                        <p className="mb-3 font-bold">institution: "BFDC (Bangladesh Film Development Corporation)"</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non totam laborum velit necessitatibus quidem fugit nesciunt corporis praesentium vero similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row items-start gap-8 mb-4 ">
+                    <div className="w-[370px] flex flex-col items-end ">
+                        <Image src={EducationLogo} alt="logo" className="education-svg" />
+                        <h3 className="text-xl text-[#ffffffda] font-bold ">loren</h3>
+                        <p className="text-sm text-[#f7f3f3b9] ">Jan 2023 - May 2024</p>
+                    </div>
+                    <div className="w-4 h-1 bg-white "></div>
+                    <div className="w-[370px] flex flex-col items-start ">
+                        <h2 className="text-lg font-bold mb-2 text-white">BSc in Computer Science & Engineering (CSE)</h2>
+                        <p className="mb-3 font-bold">institution: "Sonargaon University"</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non totam laborum velit necessitatibus quidem fugit nesciunt corporis praesentium vero similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row-reverse items-start gap-8 mb-4 ">
+                    <div className="w-[370px] flex flex-col items-start ">
+                        <Image src={EducationLogo} alt="logo" className="education-svg" />
+                        <h3 className="text-xl text-[#ffffffda] font-bold ">loren</h3>
+                        <p className="text-sm text-[#f7f3f3b9] ">Jan 2023 - May 2024</p>
+                    </div>
+                    <div className="w-4 h-1 bg-white "></div>
+                    <div className="w-[370px] flex flex-col text-end">
+                        <h2 className="text-lg font-bold mb-2 text-white">BA/BSS Programme</h2>
+                        <p className="mb-3 font-bold"> institution: "Govt. Tolaram College & University, Narayanganj"</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non totam laborum velit necessitatibus quidem fugit nesciunt corporis praesentium vero similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row items-start gap-8 mb-4 ">
+                    <div className="w-[370px] flex flex-col items-end ">
+                        <Image src={EducationLogo} alt="logo" className="education-svg" />
+                        <h3 className="text-xl text-[#ffffffda] font-bold ">loren</h3>
+                        <p className="text-sm text-[#f7f3f3b9] ">Jan 2023 - May 2024</p>
+                    </div>
+                    <div className="w-4 h-1 bg-white "></div>
+                    <div className="w-[370px] flex flex-col items-start ">
+                        <h2 className="text-lg font-bold mb-2 text-white">Studied at</h2>
+                        <p className="mb-3 font-bold">institution: "Sonargaon Govt. College"</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non totam laborum velit necessitatibus quidem fugit nesciunt corporis praesentium vero similique.
+                        </p>
                     </div>
                 </div>
             </div>
+            <div className="absolute w-[1.5px] h-[950px] bg-white "></div>
         </div>
     </div>
     )
