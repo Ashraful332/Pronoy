aea7d043fb6714854138356ee8edea6aea8266fa
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-28 17:35:10 +0600

    upgrate and try to fix

--- src/app/SmoothWrapper.tsx
+++ src/app/SmoothWrapper.tsx
@@ -1,27 +1,57 @@
-'use client'
-import { useEffect } from 'react';
+'use client';
+
+import { useEffect, useRef } from 'react';
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 export default function SmoothWrapper({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-        if (typeof window !== 'undefined' && document) {
-            const scroll = new LocomotiveScroll({
-                el: document.querySelector('[data-scroll-container]') as HTMLElement,
-                smooth: true,
-                lerp: 0.05,
-                multiplier: 2,
-            });
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-            return () => {
-                scroll.destroy();
-            };
-        }
-    }, []);
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
 
-    return (
-        <div data-scroll-container>
-            {children}
-        </div>
-    );
+    const scrollEl = scrollRef.current;
+    if (!scrollEl) return;
+
+    const scroll = new LocomotiveScroll({
+      el: scrollEl,
+      smooth: true,
+      lerp: 0.05,
+      multiplier: 2,
+    });
+
+    return () => {
+      scroll.destroy();
+    };
+  }, []);
+
+  return <div data-scroll-container ref={scrollRef}>{children}</div>;
 }
+
+
+
+// 'use client'
+// import { useEffect } from 'react';
+// import LocomotiveScroll from 'locomotive-scroll';
+// import 'locomotive-scroll/dist/locomotive-scroll.css';
+
+// export default function SmoothWrapper({ children }: { children: React.ReactNode }) {
+//     useEffect(() => {
+//         const scroll = new LocomotiveScroll({
+//         el: document.querySelector('[data-scroll-container]') as HTMLElement,
+//         smooth: true,
+//         lerp: 0.05,
+//         multiplier: 2,
+//         });
+
+//         return () => {
+//         scroll.destroy();
+//         };
+//     }, []);
+
+//     return (
+//         <div data-scroll-container>
+//         {children}
+//         </div>
+//     );
+// }
