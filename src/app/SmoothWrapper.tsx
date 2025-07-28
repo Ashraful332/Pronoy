bee844b0761c74c7fb10e3e0217fb61865460758
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-28 17:32:49 +0600

    update

--- src/app/SmoothWrapper.tsx
+++ src/app/SmoothWrapper.tsx
@@ -5,21 +5,23 @@ import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 export default function SmoothWrapper({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]') as HTMLElement,
-        smooth: true,
-        lerp: 0.05,
-        multiplier: 2,
-        });
+        if (typeof window !== 'undefined' && document) {
+            const scroll = new LocomotiveScroll({
+                el: document.querySelector('[data-scroll-container]') as HTMLElement,
+                smooth: true,
+                lerp: 0.05,
+                multiplier: 2,
+            });
 
-        return () => {
-        scroll.destroy();
-        };
+            return () => {
+                scroll.destroy();
+            };
+        }
     }, []);
 
     return (
         <div data-scroll-container>
-        {children}
+            {children}
         </div>
     );
 }
